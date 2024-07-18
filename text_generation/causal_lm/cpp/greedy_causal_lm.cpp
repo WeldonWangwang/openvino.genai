c437839e608a52c697deb2cf5aa50cbf64d3ef9f
Wangwang Wang <wangwang.wang@intel.com>
2024-07-18 10:27:32 +0800

    support warmup and num iters

--- text_generation/causal_lm/cpp/greedy_causal_lm.cpp
+++ text_generation/causal_lm/cpp/greedy_causal_lm.cpp
@@ -54,40 +54,83 @@ struct TextStreamer {
 };
 }
 
-void infer_task(ov::InferRequest lm, int64_t out_token, int64_t SPECIAL_EOS_TOKEN, size_t seq_len, TextStreamer text_streamer, ov::Tensor position_ids, float* logits, size_t vocab_size, bool enable_stream, int max_sequence_length)
+void infer_task(char* prompt, ov::CompiledModel tokenizer_compile_model, ov::CompiledModel detokenizer_compile_model, ov::CompiledModel compile_model, std::shared_ptr<ov::Model> tokenizer_model,  bool enable_stream, int max_sequence_length, int num_iters)
 {
     constexpr size_t BATCH_SIZE = 1;
-    auto start = std::chrono::system_clock::now();
-    std::vector<int64_t> out_tokens;
-    while (out_token != SPECIAL_EOS_TOKEN && seq_len < max_sequence_length) {
-        ++seq_len;
-        lm.get_tensor("input_ids").data<int64_t>()[0] = out_token;
-        lm.get_tensor("attention_mask").set_shape({BATCH_SIZE, seq_len});
-        std::fill_n(lm.get_tensor("attention_mask").data<int64_t>(), seq_len, 1);
-        position_ids.data<int64_t>()[0] = int64_t(seq_len - 1);
-        lm.start_async();
-        lm.wait();
-        out_tokens.emplace_back(out_token);
-        logits = lm.get_tensor("logits").data<float>();
-        out_token = std::max_element(logits, logits + vocab_size) - logits;
-    }
-    std::cout << "\nseq_len = " << seq_len << std::endl;
-    auto end = std::chrono::system_clock::now();
-    std::chrono::duration<double> diff = end-start;
-    std::cout << "\nRest Tokens take time: " << diff.count() << " s\n";
-    std::cout << "Rest Token/s: " << diff.count() / seq_len << std::endl;
-    if (enable_stream) {
-        for (const auto& token : out_tokens) {
-            text_streamer.put(token);
+        ov::InferRequest tokenizer = tokenizer_compile_model.create_infer_request();
+        ov::InferRequest detokenizer = detokenizer_compile_model.create_infer_request();
+        ov::InferRequest lm = compile_model.create_infer_request();
+        TextStreamer text_streamer{std::move(detokenizer)};
+        for (int i = 0; i < num_iters+1; i++) {
+            std::string log_tag = "warmup: ";
+            if (i != 0) {
+                log_tag = std::to_string(i);
+            }
+            auto [input_ids, attention_mask] = tokenize(tokenizer, prompt);
+            std::vector<int64_t> out_tokens;
+            auto seq_len = input_ids.get_size();
+            std::cout << log_tag << " input token size: " << seq_len << std::endl;
+            lm.set_tensor("input_ids", input_ids);
+            lm.set_tensor("attention_mask", attention_mask);
+            ov::Tensor position_ids = lm.get_tensor("position_ids");
+            position_ids.set_shape(input_ids.get_shape());
+            std::iota(position_ids.data<int64_t>(), position_ids.data<int64_t>() + seq_len, 0);
+            // constexpr size_t BATCH_SIZE = 1;
+            // Input values are persistent between inference calls.
+            // That allows to set values, which aren't going to change, only once
+            lm.get_tensor("beam_idx").set_shape({BATCH_SIZE});
+            lm.get_tensor("beam_idx").data<int32_t>()[0] = 0;
+            auto start = std::chrono::system_clock::now();
+            lm.infer();
+            auto end = std::chrono::system_clock::now();
+            std::chrono::duration<double> diff = end-start;
+            std::cout << log_tag <<  " First token time: " << diff.count() << " s\n";
+            size_t vocab_size = lm.get_tensor("logits").get_shape().back();
+            float* logits = lm.get_tensor("logits").data<float>() + (seq_len - 1) * vocab_size;
+            int64_t out_token = std::max_element(logits, logits + vocab_size) - logits;
+            lm.get_tensor("input_ids").set_shape({BATCH_SIZE, 1});
+            position_ids.set_shape({BATCH_SIZE, 1});
+            // Get the runtime info from the tokenizer model that we read earlier
+            auto rt_info = tokenizer_model->get_rt_info(); //Get the runtime info for the model
+            int64_t SPECIAL_EOS_TOKEN;
+
+            if (rt_info.count("eos_token_id") > 0) { //check if the runtime information has a valid EOS token ID
+                SPECIAL_EOS_TOKEN = rt_info["eos_token_id"].as<int64_t>();
+            } else {
+                throw std::runtime_error("EOS token ID not found in model's runtime information.");
+            }
+            start = std::chrono::system_clock::now();
+
+            while (out_token != SPECIAL_EOS_TOKEN && seq_len < max_sequence_length) {
+                ++seq_len;
+                lm.get_tensor("input_ids").data<int64_t>()[0] = out_token;
+                lm.get_tensor("attention_mask").set_shape({BATCH_SIZE, seq_len});
+                std::fill_n(lm.get_tensor("attention_mask").data<int64_t>(), seq_len, 1);
+                position_ids.data<int64_t>()[0] = int64_t(seq_len - 1);
+                lm.start_async();
+                lm.wait();
+                out_tokens.emplace_back(out_token);
+                logits = lm.get_tensor("logits").data<float>();
+                out_token = std::max_element(logits, logits + vocab_size) - logits;
+            }
+            std::cout << log_tag << " seq_len = " << seq_len << std::endl;
+            end = std::chrono::system_clock::now();
+            diff = end-start;
+            std::cout << log_tag << " Rest Tokens take time: " << diff.count() << " s\n";
+            std::cout << log_tag << " Rest Token/s: " << diff.count() / seq_len << std::endl;
+            if (enable_stream) {
+                for (const auto& token : out_tokens) {
+                    text_streamer.put(token);
+                }
+            }
+            text_streamer.end();
+            lm.reset_state();
         }
-    }
-    text_streamer.end();
-    lm.reset_state();
 }
 
 int main(int argc, char* argv[]) try {
-    if (argc != 6) {
-        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <MODEL_DIR> '<PROMPT>' <DEVICE_NAME> <NUM_DEVICES> <MAX_OUTPUT_TOKEN>");
+    if (argc != 7) {
+        throw std::runtime_error(std::string{"Usage: "} + argv[0] + " <MODEL_DIR> '<PROMPT>' <DEVICE_NAME> <NUM_DEVICES> <MAX_OUTPUT_TOKEN> <NUM ITERS>");
     }
 
     const std::string device_name = argv[3];
@@ -101,51 +144,18 @@ int main(int argc, char* argv[]) try {
     // tokenizer and detokenizer work on CPU only
     ov::CompiledModel tokenizer_compile_model = core.compile_model(tokenizer_model, "CPU");
     ov::CompiledModel detokenizer_compile_model = core.compile_model(std::string{argv[1]} + "/openvino_detokenizer.xml", "CPU");
+    ov::AnyMap config = {}; 
+    if (device_name.find("MULTI") == 0) {
+        config = {{"DEVICE_BIND_BUFFER","YES"},{"NUM_STREAMS","1"}};
+    }
     ov::CompiledModel compile_model = core.compile_model(
-        std::string{argv[1]} + "/openvino_model.xml", device_name, {{"DEVICE_BIND_BUFFER","YES"},{"NUM_STREAMS","1"}});
+        std::string{argv[1]} + "/openvino_model.xml", device_name, config);
     // std::vector<ov::InferRequest> tokenizer_infer_request;
     // std::vector<>
     std::vector<std::thread> threads;
     bool enable_stream = true;
     for (int i = 0; i < num_thread; i++) {
-        // ov::InferRequest tokenizer = ;
-        ov::InferRequest tokenizer = tokenizer_compile_model.create_infer_request();
-        auto [input_ids, attention_mask] = tokenize(tokenizer, argv[2]);
-        ov::InferRequest detokenizer = detokenizer_compile_model.create_infer_request();
-        ov::InferRequest lm = compile_model.create_infer_request();
-        auto seq_len = input_ids.get_size();
-        std::cout << "input token size: " << seq_len << std::endl;
-        lm.set_tensor("input_ids", input_ids);
-        lm.set_tensor("attention_mask", attention_mask);
-        ov::Tensor position_ids = lm.get_tensor("position_ids");
-        position_ids.set_shape(input_ids.get_shape());
-        std::iota(position_ids.data<int64_t>(), position_ids.data<int64_t>() + seq_len, 0);
-        constexpr size_t BATCH_SIZE = 1;
-        // Input values are persistent between inference calls.
-        // That allows to set values, which aren't going to change, only once
-        lm.get_tensor("beam_idx").set_shape({BATCH_SIZE});
-        lm.get_tensor("beam_idx").data<int32_t>()[0] = 0;
-        auto start = std::chrono::system_clock::now();
-        lm.infer();
-        auto end = std::chrono::system_clock::now();
-        std::chrono::duration<double> diff = end-start;
-        std::cout << "First token time: " << diff.count() << " s\n";
-        size_t vocab_size = lm.get_tensor("logits").get_shape().back();
-        float* logits = lm.get_tensor("logits").data<float>() + (seq_len - 1) * vocab_size;
-        int64_t out_token = std::max_element(logits, logits + vocab_size) - logits;
-        lm.get_tensor("input_ids").set_shape({BATCH_SIZE, 1});
-        position_ids.set_shape({BATCH_SIZE, 1});
-        TextStreamer text_streamer{std::move(detokenizer)};
-        // Get the runtime info from the tokenizer model that we read earlier
-        auto rt_info = tokenizer_model->get_rt_info(); //Get the runtime info for the model
-        int64_t SPECIAL_EOS_TOKEN;
-
-        if (rt_info.count("eos_token_id") > 0) { //check if the runtime information has a valid EOS token ID
-            SPECIAL_EOS_TOKEN = rt_info["eos_token_id"].as<int64_t>();
-        } else {
-            throw std::runtime_error("EOS token ID not found in model's runtime information.");
-        }
-        threads.emplace_back(std::thread(infer_task, lm, out_token, SPECIAL_EOS_TOKEN, seq_len, text_streamer, position_ids, logits, vocab_size, enable_stream, max_output_token_nums));
+        threads.emplace_back(std::thread(infer_task, argv[2], tokenizer_compile_model, detokenizer_compile_model, compile_model, tokenizer_model, enable_stream, max_output_token_nums, std::stoi(argv[6])));
         enable_stream = false;
     }
 
